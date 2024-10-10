9157cd190c027734942b2684fd64d79799c6b667
wwf9w0 <aliemrekaradag1@gmail.com>
2024-10-10 18:14:17 +0700

    initial commit

---
+++ src/main/java/com/devops/automation/service/DevopsAutomationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devops.automation.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsAutomationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsAutomationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/automation/service/DevopsAutomationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.automation.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAutomationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
